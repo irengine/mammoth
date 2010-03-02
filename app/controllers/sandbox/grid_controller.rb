dbf0f7e7aee4e828e16fdd055925ca9c06a245a8
Alex Tang <lalextang@msn.com>
2010-03-03 01:38:50 +0800

    complex sql support

--- app/controllers/sandbox/grid_controller.rb
+++ app/controllers/sandbox/grid_controller.rb
@@ -48,4 +48,61 @@ CM
     render :text => column_model
   end
 
+  def show
+    initial_data
+    @sql = get_sql
+    @result = ActiveRecord::Base.connection.execute(@sql)
+  end
+
+  private
+  def initial_data
+    @syntax = {}
+
+    select = {}
+    from = {}
+    join_condition = []
+    query_condition = []
+
+    # select
+    select['user.id'] = {}
+    select['user.name'] = {}
+    select['department.name'] = {}
+    select['user_1.name'] = {}
+
+    # from alias_name => table_name
+    from['user'] = 'users'
+    from['department'] = 'departments'
+    from['user_1'] = 'users'
+
+    #join
+    join_condition << ' and user.department_id = department.id'
+    join_condition << ' and department.manager_id = user_1.id'
+
+    @syntax[:select] = select
+    @syntax[:from] = from
+    @syntax[:join_condition] = join_condition
+    @syntax[:query_condition] = query_condition
+  end
+
+  def get_sql
+    sql = ''
+    sql << 'select '
+    @syntax[:select].keys.each do |k|
+      sql << "#{k},"
+    end
+
+    sql = sql.chop
+    sql << ' from '
+    @syntax[:from].each do |k,v|
+      sql << "#{v} as #{k},"
+    end
+
+    sql = sql.chop
+    sql << ' where 1=1'
+    @syntax[:join_condition].each do |c|
+      sql << c
+    end
+    return sql.to_s
+  end
+
 end
